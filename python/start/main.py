9f541b7fe693cd234206bdd08f73b7035640d159
Ransilu456 <samarasekarakeshan@gmail.com>
2026-03-10 05:51:54 +0530

    Creaated some patterns

--- python/start/main.py
+++ python/start/main.py
@@ -1,3 +1,32 @@
 print("Hello World")
 
-print("This is my first python programe")
+# text = "This is my first python programme."
+# print(f"This is the before text: {text}")
+
+# new_text = text.split(" ")
+# new_upper = text.upper()
+
+# print(f"This is after text 1: {new_text}")
+# print(f"This is after text 2: {new_upper}")
+
+
+# user_input = input("Enter Your name: ")
+# print(f"Hello {user_input}")
+
+rows = 25
+
+#Left to right
+for i in range(1, rows + 1):
+    j = 1
+    while j <= i:
+        print("*", end="")
+        j = j + 1
+
+    print("")
+
+per_line = 25
+
+for i in range(1, rows - 1):
+    for j in range(i, per_line):
+        print("*", end="")
+    print("")
